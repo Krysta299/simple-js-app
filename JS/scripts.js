2647dc582c8613228cd7ddcb5477d604e09b48f7
Krysta_P <124639769+Krysta299@users.noreply.github.com>
2023-10-30 13:35:49 -0400

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,41 +1,57 @@
-let pokemonRepository = (function(){
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {
+            name: 'Butterfree',
+            height: 1.1,
+            types: ['bug', 'flying'],
+        },
+        {
+            name: 'Charizard',
+            height: 1.7,
+            types: ['fire', 'flying'],
+        },
+        {
+            name: 'Venusaur',
+            height: 2,
+            types: ['grass', 'poison'],
+        }
+    ];
 
-    let message = 'Wow, that’s big!'; // Define the message variable
+    let pokemonListElement = document.querySelector('.pokemonList'); 
 
-    let pokemonList= [
-        {name: 'Butterfree' , 
-        height: 1.1 , 
-        types: ['bug' , 'flying']
-        } ,
-        {name: 'Charizard' , 
-        height: 1.7 , 
-        types: ['fire' , 'flying']
-        } ,
-        {name: 'Vennsaur' , 
-        height: 2 ,  
-        types: ['grass' , 'poison']
-    }];
+    function addButtonEventListener(button, pokemon) {
+        button.addEventListener('click', function() {
+            showDetails(pokemon);
+        });
+    }
+
+    function addListItem(pokemon) {
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name; 
+        button.classList.add('selected');
+        listItem.appendChild(button);
+        pokemonListElement.appendChild(listItem);
+
+        addButtonEventListener(button, pokemon);
+    
+    function showDetails(pokemon) {
+        console.log(pokemon.name);
+        button.addEventListener('click', function() {
+        showDetails(pokemon);
+        });
+    }}
+      
+    function getAll() {
+        return pokemonList;
+        document.querySelector('.pokemon-list').appendChild(pokemonListElement);
+    }
+
+    pokemonList.forEach(addListItem);
 
     return {
-        getAllPokemonList: function(){
-            return pokemonList;
-        },
-        addPokemon: function(pokemon){
-           let expectedKeys = ['name' , 'height' , 'types'];
-           let providedKeys = Object.keys(pokemon);
-           let keysMatch = expectedKeys.every((key) => providedKeys.includes(key))
-           if(keysMatch){
-           pokemonList.push(pokemon);
-            document.write('<p>' + pokemon.name + '-' + 'Height:' + pokemon.height + '</p>');
-            if (pokemon.height < 1.5 && pokemon.height >= 1) {
-                console.log('this is a small pokemon');
-            } else if (pokemon.height < 2) {
-                console.log('this is an average pokemon');
-            } else {
-                console.log('this is a big pokemon');
-                document.write('<p>' + pokemon.name + '-' + message + '</p>');
-            }
-        }else {
-            console.error('Invalid Pokemon object. It must have the following keys: ' + expectedKeys.join(', '));
-        }
-    }}()})
+        getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails
+    };
+})();
